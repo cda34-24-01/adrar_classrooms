7b6f283f1820edef10cd8cb5a4e0e20471b610b4
facubotta <nutuestampados@gmail.com>
2024-10-07 16:29:22 +0200

    j'ai adapte mon code en fonction de l'API mais on n'a pas encore des donnes alors il faut attandre pour le tester

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CoursController extends AbstractController
 {
-    private array $courses = [
+    private array $courses2 = [
         [
             'id' => 1,
             'title' => 'C++ Fundamentals',
@@ -246,21 +248,191 @@ class CoursController extends AbstractController
         ],
     ];
 
+    private $httpClient;
+    private $courses;
+    private $languages;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    private function getAllCourses(): array
+    {
+        if (!$this->courses) {
+            $response = $this->httpClient->request('GET', 'https://127.0.0.1:8001/api/cours', [
+                'verify_peer' => false,
+            ]);
+            $statusCode = $response->getStatusCode();
+            if ($statusCode !== 200) {
+                throw new \Exception('Error getting all courses.');
+            }
+            $data = $response->toArray();
+            $courses = $data['member'];
+
+            foreach ($courses as &$course) {
+                $chapterUrls = $course['chapter'];
+                $chapterIds = [];
+
+                foreach ($chapterUrls as $chapterUrl) {
+                    $parts = explode('/', $chapterUrl);
+                    $chapterIds[] = end($parts);
+                }
+
+                $course['chaptersIds'] = $chapterIds;
+            }
+
+            $this->courses = $courses;
+        }
+
+        return $this->courses;
+    }
+    private function getCourseById(int $id): array
+    {
+        try {
+            $response = $this->httpClient->request('GET', 'https://127.0.0.1:8001/api/cours/' . $id, [
+                'verify_peer' => false,
+            ]);
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode !== 200) {
+                throw new \Exception('Error getting course by id.');
+            }
+
+            $data = $response->toArray();
+
+            return $data;
+        } catch (\Exception $e) {
+            dump('Error: ' . $e->getMessage());
+        }
+    }
+    private function getLanguagesWithCourses(): array
+    {
+        $languages = [];
+
+        if (!$this->languages) {
+            $response = $this->httpClient->request('GET', 'https://127.0.0.1:8001/api/languages', [
+                'verify_peer' => false,
+            ]);
+            $statusCode = $response->getStatusCode();
+            if ($statusCode !== 200) {
+                throw new \Exception('Error getting all languages.');
+            }
+
+            $data = $response->toArray();
+
+            foreach ($data['member'] as $language) {
+                if (isset($language['cours']) && count($language['cours']) > 0) {
+                    $languages[] = $language;
+                }
+            }
+
+            $this->languages = $languages;
+        }
+
+        return $this->languages;
+    }
+    private function getCoursesByLanguage($language): array
+    {
+        $courses = [];
+
+        foreach ($language['cours'] as $courseUrl) {
+            try {
+                $response = $this->httpClient->request('GET', 'https://127.0.0.1:8001' . $courseUrl, [
+                    'verify_peer' => false,
+                ]);
+
+                $statusCode = $response->getStatusCode();
+
+                if ($statusCode !== 200) {
+                    throw new \Exception('Error getting courses by language.');
+                }
+
+                $data = $response->toArray();
+
+                $chapterUrls = $data['chapter'];
+                $chapterIds = [];
+
+                foreach ($chapterUrls as $chapterUrl) {
+                    $parts = explode('/', $chapterUrl);
+                    $chapterIds[] = end($parts);
+                }
+
+                $data['chaptersIds'] = $chapterIds;
+
+                $courses[] = $data;
+            } catch (\Exception $e) {
+                dump('Error: ' . $e->getMessage());
+            }
+        }
+
+        return $courses;
+    }
+    private function getChaptersByCourse($chaptersUrls): array
+    {
+        $chapters = [];
+        foreach ($chaptersUrls as $chapterUrl) {
+            try {
+                $response = $this->httpClient->request('GET', 'https://127.0.0.1:8001' . $chapterUrl, [
+                    'verify_peer' => false,
+                ]);
+                $statusCode = $response->getStatusCode();
+                if ($statusCode !== 200) {
+                    throw new \Exception('Error getting chapters by course.');
+                }
+                $data = $response->toArray();
+                $chapters[] = $data;
+            } catch (\Exception $e) {
+                dump('Error: ' . $e->getMessage());
+            }
+        }
+        return $chapters;
+    }
+    private function getChapterById($chapterId): array
+    {
+        try {
+            $response = $this->httpClient->request('GET', 'https://127.0.0.1:8001/api/chapters/' . $chapterId, [
+                'verify_peer' => false,
+            ]);
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode !== 200) {
+                throw new \Exception('Error getting chapter by id.');
+            }
+
+            $data = $response->toArray();
+
+            return $data;
+        } catch (\Exception $e) {
+            dump('Error: ' . $e->getMessage());
+        }
+    }
+
     #[Route('/cours', name: 'app_cours')]
     public function index(Request $request): Response
     {
+        $courses = $this->getAllCourses();
+        $languages = $this->getLanguagesWithCourses();
 
-        $selected_language = $request->query->get('language');
+        $selected_language_title = $request->query->get('language');
 
-        $filtered_courses = $this->courses;
-
-        if ($selected_language) {
-            $filtered_courses = array_filter($filtered_courses, function ($course) use ($selected_language) {
-                return $course['language'] === $selected_language;
-            });
+        $selected_language = null;
+        foreach ($languages as $language) {
+            if ($language['title'] === $selected_language_title) {
+                $selected_language = $language;
+                break;
+            }
         }
 
-        $languages = array_unique(array_column($this->courses, 'language'));
+        $filtered_courses = $courses;
+
+        if ($selected_language) {
+            $filtered_courses = $this->getCoursesByLanguage($selected_language);
+        }
+        dump($filtered_courses);
+
 
         return $this->render('cours/index.html.twig', [
             'controller_name' => 'CoursController',
@@ -273,39 +445,36 @@ class CoursController extends AbstractController
     #[Route('/cours/{courseId}/{chapterId}', name: 'app_chapter_show', methods: ['GET'])]
     public function show_chapter($courseId, $chapterId): Response
     {
-        $selectedCourse = null;
-        foreach ($this->courses as $course) {
-            if ($course['id'] == $courseId) {
-                $selectedCourse = $course;
-                break;
-            }
-        }
 
+        $selectedCourse = $this->getCourseById($courseId);
         if (!$selectedCourse) {
             throw $this->createNotFoundException('Course not found.');
         }
 
+        $chapters = $this->getChaptersByCourse($selectedCourse['chapter']);
+        if (!$chapters) {
+            // throw $this->createNotFoundException('Chapters not found.');
+            $chapters = [];
+        }
+
+
+        $selectedCourse['chapter'] = $chapters;
+        dump($selectedCourse);
+
         $currentChapter = null;
-        foreach ($selectedCourse['chapters'] as $chapter) {
-            if ($chapter['id'] == $chapterId) {
-                $currentChapter = $chapter;
-                break;
-            }
+        if ($chapterId !== 'noChapters') {
+            $currentChapter = $this->getChapterById($chapterId);
         }
 
-        if (!$currentChapter) {
-            throw $this->createNotFoundException('Chapter not found.');
-        }
+        $chapterCount = count($chapters);
+        $currentIndex = array_search($chapterId, array_column($chapters, 'id'));
 
-        $chapterCount = count($selectedCourse['chapters']);
-        $currentIndex = array_search($chapterId, array_column($selectedCourse['chapters'], 'id'));
-
-        $previousChapter = ($currentIndex > 0) ? $selectedCourse['chapters'][$currentIndex - 1] : null;
-        $nextChapter = ($currentIndex < $chapterCount - 1) ? $selectedCourse['chapters'][$currentIndex + 1] : null;
+        $previousChapter = ($currentIndex > 0) ? $chapters[$currentIndex - 1] : null;
+        $nextChapter = ($currentIndex < $chapterCount - 1) ? $chapters[$currentIndex + 1] : null;
 
         return $this->render('cours/show_course.html.twig', [
             'course' => $selectedCourse,
-            'chapters' => $selectedCourse['chapters'],
+            'chapters' => $chapters,
             'chapter' => $currentChapter,
             'previousChapter' => $previousChapter,
             'nextChapter' => $nextChapter,
