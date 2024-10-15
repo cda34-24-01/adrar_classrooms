3a55ab44b3b7f0c1f08b66cd570b636a0af8e3f8
MathisAdrar <mathisb.buisson@gmail.com>
2024-10-15 15:50:26 +0200

    Changed CSS for better contrast, added randomisation of quotes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
-use DateTime;
 use DateTimeZone;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,10 +11,37 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 
 class RegistrationController extends AbstractController
 {
+    private $httpClient;
+    private $users;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    private function getAllUsers(): array
+    {
+        if (!$this->users) {
+            $response = $this->httpClient->request('GET', 'https://127.0.0.1:8001/api/users', [
+                'verify_peer' => false,
+            ]);
+            $statusCode = $response->getStatusCode();
+            if ($statusCode !== 200) {
+                throw new \Exception('Error getting all users.');
+            }
+            $data = $response->toArray();
+            $users = $data['member'];
+
+            $this->users = $users;
+        }
+
+        return $this->users;
+    }
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
@@ -40,8 +66,16 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_api_index');
         }
 
+
+        $users = $this->getAllUsers();
+
+        $filtered_users = $users;
+        $userPick = rand(0, count($filtered_users) - 1);
+        
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
+            'users' => $filtered_users,
+            'userPick' => $userPick
         ]);
     }
 }
